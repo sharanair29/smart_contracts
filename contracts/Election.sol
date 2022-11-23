ff1e634cbcbdc5773a8e2ba49f835872a44f85a5
Akshara Sadheesh <94154731+sharanair29@users.noreply.github.com>
2022-11-24 06:16:41 +0800

    24/11

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,44 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
-    //Store candidate
-    
-    //Read candidate
-    string public candidate;
-    //Constructor
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //Store accounts that have voted
+    mapping(address => bool) public voters;
+
+    // Store Candidates
+
+    // Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    // Store Candidates Count
+    uint public candidatesCount;
     constructor () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+        // candidate = "Candidate 1";
     }
 
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+    
+    function vote (uint _candidateId) public {
+        // required that they have not voted before
+        require(!voters[msg.sender]);
+        // required a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+        // msg.sender
+        // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+    }
+    
+    
 }
