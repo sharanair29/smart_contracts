c2db42dc24052e767701fc814d538055159d70f8
Akshara Sadheesh <94154731+sharanair29@users.noreply.github.com>
2022-11-23 11:33:39 +0800

    first commit

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.22 <0.8.0;
+
+contract Election {
+    //Store candidate
+    
+    //Read candidate
+    string public candidate;
+    //Constructor
+    constructor () public {
+        candidate = "Candidate 1";
+    }
+
+}
